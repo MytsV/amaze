c1b8fb3f85556995e1ae2c72f6e59da6d7e2ca6d
MytsV <mytsvictoria@gmail.com>
2023-05-19 22:26:12 +0300

    Initialize client project
    Use create-react-app script to initialize a template React app.
    Tweak the template to be as simple as possible.

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import App from './App';
+
+test('Renders hello world text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hello world!/i);
+  expect(linkElement).toBeInTheDocument();
+});
