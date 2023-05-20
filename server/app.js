c5e83feb1231bd89e9350a6bdf31a11ea83d637e
MytsV <mytsvictoria@gmail.com>
2023-05-20 17:14:11 +0300

    Set up client-server communication
    Tweak the client to receive text from the server. Modify start
    script to pass SERVER_PORT to the React app. Include CORS policy
    in server app.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config({path: '../.env'});
 
 const app = express();
 const port = process.env.SERVER_PORT || 8080;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
