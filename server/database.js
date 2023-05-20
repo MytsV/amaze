ea6d1ccb6241255a628372da56e37a54035e33ce
MytsV <mytsvictoria@gmail.com>
2023-05-20 20:33:42 +0300

    Split up environment
    Create .env.development and .env.production files. Add them to
     .gitignore. Change scripts and imports accordingly.

--- server/database.js
+++ server/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({path: '../.env'});
+require('dotenv').config({path: `../.env.${process.env.NODE_ENV}`});
 
 mongoose.connect(process.env.DB_URL);
 
