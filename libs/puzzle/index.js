f9db8136b3ac364ae5dd7cca41463c4539ca1612
MytsV <mytsvictoria@gmail.com>
2023-06-08 21:25:35 +0300

    Make export new modifiers from the library

--- libs/puzzle/index.js
+++ libs/puzzle/index.js
@@ -1,7 +1,20 @@
 const {Maze, EdgeType} = require('./src/maze');
 const Position = require('./src/position');
-const {HexagonModifier} = require('./src/modifier');
+const {
+  HexagonModifier,
+  StarModifier,
+  SquareModifier,
+} = require('./src/modifier');
 const {Path} = require('./src/path');
 const {Solution} = require('./src/solution');
 
-module.exports = {Maze, Position, EdgeType, HexagonModifier, Path, Solution};
+module.exports = {
+  Maze,
+  Position,
+  EdgeType,
+  HexagonModifier,
+  SquareModifier,
+  StarModifier,
+  Path,
+  Solution,
+};
